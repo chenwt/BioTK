7ca8f954200ad60dec039570b47f2cda10b4e2c1
Cory Giles <mail@corygil.es>
2014-04-03 14:31:25 -0500

    Use x_label and y_label instead of xlabel/ylabel in plotting functions.

--- BioTK/plot/__init__.py
+++ BioTK/plot/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from .heatmap import heatmap
 
-def histogram(data, bins=20, title=None, xlabel=None, ylabel=None):
+def histogram(data, bins=20, title=None, x_label=None, y_label=None):
     """
     Create a histogram from a collection of data points.
     """
@@ -12,8 +12,8 @@ def histogram(data, bins=20, title=None, xlabel=None, ylabel=None):
     center = (bins[:-1] + bins[1:]) / 2
     if title:
         plt.title(title)
-    if xlabel:
-        plt.xlabel(xlabel)
-    if ylabel:
-        plt.ylabel(ylabel)
+    if x_label:
+        plt.xlabel(x_label)
+    if y_label:
+        plt.ylabel(y_label)
     return plt.bar(center, hist, align="center", width=width)
