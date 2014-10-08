95695d69765eb2c12cef46d65580d0ddc0d9fb94
Cory Giles <mail@corygil.es>
2014-10-07 19:26:05 -0500

    Move util/ scripts to script/ folder and fold transpose-block into transpose

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -7,11 +7,12 @@ pkgs=(
     pixz pigz
     liblz4-tool
     python3-numpy python3-scipy python3-matplotlib
-    python3-pandas
+    python3-pandas python3-psycopg2
 )
 
 sudo apt-get install -y ${pkgs[@]}
 
-for pkg in $(cat $(dirname $0)/../requirements.txt); do
+for pkg in $(cat $(dirname $0)/../requirements.txt \
+    | grep -v '#' | grep -v '^$'); do
     sudo pip3 install $pkg
 done
