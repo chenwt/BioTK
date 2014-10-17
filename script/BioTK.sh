d89e215c9557d9ff0b8793d5273f037e649d2723
Cory Giles <mail@corygil.es>
2014-10-17 16:31:37 -0500

    Add caching helpers to bash library.

--- script/BioTK.sh
+++ script/BioTK.sh
@@ -4,6 +4,21 @@
 # Configuration
 ###############
 
+log() {
+    local OPTIND
+    tag=BioTK
+    while getopts :t: opt; do
+        case $opt in
+            t) tag="$OPTARG" ;;
+            *) opts+="-$opt $OPTARG" ;;
+        esac
+    done
+    which logger 2> /dev/null && {
+        logger "$@"
+    }
+}
+export -f log
+
 cfg() {
     # Locate the BioTK configuration file and query it for the given key
 
@@ -79,3 +94,38 @@ lzpaste() {
     rm -rf $tmpdir
 }
 export -f lzpaste
+
+#########
+# Caching
+#########
+
+cache_download() {
+    cache="$CACHE"/download
+    mkdir -p "$cache"
+    url="$1"
+    name="$(echo "$url" | base64)"
+    path="$cache/$name"
+    if [ ! -f $path ]; then
+        if which aria2c &> /dev/null; then
+            aria2c -d "$cache" -o "$name" "$url"
+        else
+            curl -o "$path" "$url"
+        fi
+    fi &> /dev/null
+    cat "$path"
+}
+export -f cache_download
+
+memoize() {
+    # Given a function (name), and optionally arguments,
+    # execute the function and cache the result as a file.
+    cache="$CACHE/memoize"
+    mkdir -p "$cache"
+    path="$cache/$(echo "$@" | base64)"
+    if [ ! -f "$path" ]; then
+        "$@" | tee >(gzip > "$path")
+    else
+        zcat "$path"
+    fi
+}
+export -f memoize
